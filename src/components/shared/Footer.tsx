2a466a0a10e4094b8ccc42489f2ccdb1fd2a7c02
immanuel <majekodunmiimmanuel9@gmail.com>
2025-10-12 15:20:35 +0100

    minor fix

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -5,7 +5,7 @@ import logo from '@/svgs/logo.svg'
 
 const Footer = () => {
     return (
-        <div className='bg-[linear-gradient(90deg,#0D0D0D_0%,#0D0D0D_55%,#0D0D0D_100%)] z-40 rounded-[40px] h-[500px] xl:mx-72 mx-12 relative overflow-auto mt-20 mb-14'>
+        <div className='bg-[linear-gradient(90deg,#0D0D0D_0%,#0D0D0D_55%,#0D0D0D_100%)] z-40 rounded-[40px] h-[500px] xl:mx-60 mx-12 relative overflow-auto mt-20 mb-14'>
 
 
 
@@ -28,7 +28,7 @@ const Footer = () => {
                 <div className='backdrop-blur-[16px] w-[500px] xl:w-[600px] xl:px-7 py-[18px] rounded border border-[#FEFEFE]  flex items-center justify-between'>
                     <input className=' lg:ml-4 xl:ml-0 text-white lg:text-xs xl:text-xl w-[400px] focus:border-0 focus:outline-none' type="text" placeholder='  Sign up for our newsletter and join the growing Decane community.' />
 
-                    <div className='flex items-center justify-center gap-2 bg-black xl:py-3 xl:px-8 py-2 px-4 rounded-xl cursor-pointer lg:mr-4 xl:mr-0'>
+                    <div className='flex items-center justify-center gap-2 bg-black xl:py-3 xl:px-8 py-2 px-2 rounded-xl cursor-pointer lg:mr-4 xl:mr-0'>
                         <p className='font-tajawal font-normal text-white text-xs'>Sign Up</p>
                     </div>
                 </div>
